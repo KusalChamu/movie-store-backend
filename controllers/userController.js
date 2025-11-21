d0c289257949c8c56df8f6da677c2402aabb4db5
KusalChamu <chamuchamp09@gmail.com>
2025-11-21 16:48:55 +0530

    implement create user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,17 @@
+export function createUser(req,res){
+    const role = req.body.role || "customer"
+
+    if(role === "admin"){
+        if(!req.user || req.user.role !=="admin"){
+            return res.status(403).json({
+                message: "Unauthorized: only admins can create another admin"
+            });
+        }
+    }
+
+    
+}
+
 export function isAdmin(req){
     if(!req.user) return false;
     return req.user.role === "admin"
