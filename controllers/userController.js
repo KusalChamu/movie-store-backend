7eb6f51973b3378df2958d2642ae006c645642bc
KusalChamu <chamuchamp09@gmail.com>
2025-11-24 09:55:06 +0530

    implement login user

--- controllers/userController.js
+++ controllers/userController.js
@@ -56,6 +56,42 @@ user.save()
         });
 }
 
+export function loginUser(req,res) {
+    const { email,password } = req.body
+
+    User.findOne({ email: email }).then((user)=>{
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+        if (isPasswordCorrect) {
+            const token = jwt.sign(
+                {
+                    email: user.email,
+                    firstName: user.firstName,
+                    lastName: user.lastName,
+                    role: user.role,
+                    img: user.img
+                },
+                process.env.JWT_KEY
+            )
+            res.status(200).json({
+                message: "Login successful",
+                token: token,
+                role: user.role
+            });
+        } else {
+            res.status(401).json({
+                message: "Invalid password"
+            });
+        }
+    })
+}
+        
+
 export function isAdmin(req){
     if(!req.user) return false;
     return req.user.role === "admin"
